0745ffe8ad21e6af67bfbed6d527570d67fb27b0
Thanh Huy Nguyen <huy.nt.0911@gmail.com>
2021-01-14 19:35:21 +0700

    edit 1 3

--- exercise01.R
+++ exercise01.R
@@ -3,22 +3,23 @@ playBill<-read.csv('datasource\\playbill.csv', header = TRUE, sep = ",")
 dim(playBill)
 summary(playBill)
 head(playBill)
-playBill[1:15,]
-plot(playBill)
-
-
-#aaa
-M1 <- lm(playBill$sales ~ playBill$)
-
-
-#M1 <- lm(Advertising$sales ~ Advertising$TV)
-# summary(M1)
-# names(M1)
-# coef(M1)
-# confint(M1)
-
-# plot( Advertising$TV,Advertising$sales)
-# abline(M1,lwd=3,col="red")
-
-# par(mfrow=c(2,2))
-# plot(M1)
+playBill[1:5,]
+#plot(playBill)
+#1a
+M1 <- lm(CurrentWeek ~ LastWeek, data = playBill)
+M1smr <- summary(M1)
+M1smr
+b0<-summary(M1)$coefficients[1,1]
+b1<-summary(M1)$coefficients[2,1]
+b1thesis <- 1
+seb1 <-summary(M1)$coefficients[2,2]
+pvalue1a <- 2*pt(b1-b1thesis/seb1,n-2)
+cat(pvalue1a)
+#1b
+b0thesis <- 10000
+seb0 <-summary(M1)$coefficients[1,2]
+pvalue1b <- 2*pt(b0-b0thesis/seb0,n-2)
+cat(pvalue1b)
+#1c
+prd1c <- predict.lm(M1,data.frame(LastWeek=c(400000)),interval = "confidence", level = 0.95)
+cat(prd1c)
--- exercise03.R
+++ exercise03.R
@@ -14,6 +14,7 @@ b1<-summary(M3)$coefficients[2,1]
 seb1 <-summary(M3)$coefficients[2,2]
 b1thesis <- 0.01
 pvalue3b <- 2*pt(b1-b1thesis/seb1,n-2)
+cat(pvalue3b)
 #3b
 #x  = data.frame(Invoices=c(130,130))
 #predict.lm(M3,x,interval = "confidence", level = 0.95)
@@ -24,3 +25,4 @@ ystar = b0+b1*xstar
 alpha = 0.05
 lowerY <- ystar - modelSigma*sqrt(1/30)*qt(p=1-alpha/2,df=n-2)
 upperY <- ystar + modelSigma*sqrt(1/30)*qt(p=1-alpha/2,df=n-2)
+cat(lowerY, ystar,upperY)
