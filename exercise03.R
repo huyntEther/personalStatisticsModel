1cfb3afe8763148918429faa28aa38b2599ec123
HuyNguyen <huy.nguyen@svtech.com.vn>
2021-01-14 18:31:09 +0700

    mod e3

--- exercise03.R
+++ exercise03.R
@@ -1,23 +1,26 @@
 invoices<-read.table('datasource\\invoices.txt', header = TRUE, sep = "\t")
-head(invoices)
-summary(invoices)
-invoices[1:5,]
+#head(invoices)
+#summary(invoices)
+#invoices[1:5,]
 # plot(invoices)
-plot(invoices$Invoices,invoices$Time)
-
-M3 <- lm(Time~Invoicesdata = invoices )
-M3smr<-summary(Mb)
+#plot(invoices$Invoices,invoices$Time)
+#get var from model
+M3 <- lm(Time~Invoices,data = invoices )
+M3smr<-summary(M3)
+n <-length(M3$residuals)
+b0<-summary(M3)$coefficients[1,1]
+b1<-summary(M3)$coefficients[2,1]
 #3b
-# M3smr
-# M3smr$coefficients
-# thesisSlope = 0.01
-# tstatsSlope <- (thesisSlope - M3smr$coefficients[2,1])/M3smr$coefficients[2,2]
-# tstatsSlope
-# pvalSlope <- 2* pt(abs(tstatsSlope),df=df.residual(M3), lower.tail = FALSE)
-# pvalSlope2 <- 2* pt(abs(tstatsSlope),df=df.residual(M3), lower.tail = TRUE)
-# pvalSlope
-# pvalSlope2
-
+seb1 <-summary(M3)$coefficients[2,2]
+b1thesis <- 0.01
+pvalue3b <- 2*pt(b1-b1thesis/seb1,n-2)
 #3b
-x  = data.frame(Invoices=c(130,130))
-predict.lm(M4,x,interval = "confidence")
+#x  = data.frame(Invoices=c(130,130))
+#predict.lm(M3,x,interval = "confidence", level = 0.95)
+
+xstar = 130
+modelSigma <- M3smr$sigma
+ystar = b0+b1*xstar
+alpha = 0.05
+lowerY <- ystar - modelSigma*sqrt(1/30)*qt(p=1-alpha/2,df=n-2)
+upperY <- ystar + modelSigma*sqrt(1/30)*qt(p=1-alpha/2,df=n-2)
